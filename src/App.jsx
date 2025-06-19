2af770f0df1eaa6a23dcb489b95d1b6c7375277b
chetan.sharma.83@outlook.com <chetan sharma>
2025-06-19 22:39:42 +0530

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,25 @@ function App() {
   const [mainImage, setMainImage] = useState("");
 
   return (
-    <div className="relative h-screen w-screen overflow-hidden">
-      <Background />
-      
-      <Countdown />
-      <MusicToggle />
-      <SocialLinks/>
-      <div className="relative h-full w-full flex items-center  px-4">
-  <QuoteSection quote={quote} />
-  <MainImage src={mainImage} />
-</div>
+    <div className="relative w-screen h-screen overflow-hidden flex flex-col">
+      {/* Scrollable content */}
+      <div className="flex-1 overflow-y-auto">
+        <Background />
+        <Countdown />
+        <MusicToggle />
+        <SocialLinks />
+        <div className="relative h-full w-full flex items-center px-4">
+          <QuoteSection quote={quote} />
+          <MainImage src={mainImage} />
+        </div>
+      </div>
 
+      {/* Fixed bottom carousel */}
       <Carousel setMainImage={setMainImage} setQuote={setQuote} />
       <PhotoModal />
     </div>
   );
 }
+
 
 export default App;
